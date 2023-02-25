feaf025f90a4c8f3eecbe9a63fccad5316b2bf64
Jmontielll <kinoamontiel@gmail.com>
2023-02-25 13:43:12 -0800

    2-print

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,33 +1,33 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
 	int n;
-	
- srand(time(0));
- n = rand() - RAND_MAX / 2;
 
-   if (n > 0)
-   {
-		printf("%d is positive\n", n);
-   }
-   else if (n == 0)
-    {
-		printf("%d is zero\n", n);
-    }
-    else if (n < 0)
-    {
-        printf("%d is negative\n", n);
-    }
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-   return (0);
+	if (n > 0)
+	{
+		printf("%d%s\n", n, " is positive");
+	}
+	else if (n == 0)
+	{
+		printf("%d%s\n", n, " is zero");
+	}
+	else
+	{
+		printf("%d%s\n", n, " is negative");
+	}
+
+	return (0);
 }
 
---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char js;
+
+	for (js = 'a'; js <= 'z'; ++js)
+		putchar(js);
+	putchar('\n');
+
+	return (0);
+}
