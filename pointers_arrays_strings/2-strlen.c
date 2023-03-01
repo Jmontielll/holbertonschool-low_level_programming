4d46c187f35b79e7977c94ffea75d1394aee495b
Jmontielll <kinoamontiel@gmail.com>
2023-03-01 12:08:48 -0800

    ejercicio_2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; s[c]; s++)
+	{
+		c++;
+	}
+	return (c);
+}
