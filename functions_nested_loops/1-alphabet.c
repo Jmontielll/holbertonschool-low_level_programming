2c02a36946f839175fc325f37a92496c73d8ab8f
Jmontielll <kinoamontiel@gmail.com>
2023-03-03 12:37:37 -0800

    ejercicio_1.1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <main.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void);
+{
+	char c4;
+
+	for (c4 = 'a'; c4 <= 'z'; ++c4)
+		putchar(c4);
+
+	
+	putchar('\n');
+
+	return (0);
+}
