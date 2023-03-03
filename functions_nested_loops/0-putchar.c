c9c8f4435fe816e0b5cf4e001a447df598a976ee
Jmontielll <kinoamontiel@gmail.com>
2023-03-03 13:36:25 -0800

    ejercicio_0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -6,16 +6,15 @@
  */
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t'); 	   
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
 
-    return (0);
+putchar('_')
+putchar('p')
+putchar('u')
+putchar('t')
+putchar('c')
+putchar('h')
+putchar('a')
+putchar('r')
+	return (0);
 }
 
